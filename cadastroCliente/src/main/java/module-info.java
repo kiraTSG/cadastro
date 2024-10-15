ed3a1b1eacb341b7c9a09570e7d28a8b86cc2724
kiraTSG <102935872+kiraTSG@users.noreply.github.com>
2024-10-14 21:44:14 -0300

    Delete cadastroCliente directory

--- cadastroCliente/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    opens com to javafx.fxml;
-    exports com;
-    requires com.google.gson;
-    requires org.apache.httpcomponents.httpcore;
-}
